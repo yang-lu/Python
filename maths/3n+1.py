68e9ea3f69296a63ae29bc0b412f60b662cfc9fe
Yang Lu <yang.lu@outlook.com>
2019-05-22 00:07:51 -0700

    Add spaces for comments

--- maths/3n+1.py
+++ maths/3n+1.py
@@ -3,11 +3,11 @@ def main():
         c = 0
         l = [a]
         while a != 1:
-            if a % 2 == 0:#if even divide it by 2
+            if a % 2 == 0:  #if even divide it by 2
                 a = a // 2
-            elif a % 2 == 1:#if odd 3n+1
+            elif a % 2 == 1:    #if odd 3n+1
                 a = 3*a +1
-            c += 1#counter
+            c += 1  #counter
             l += [a]
 
         return l , c
